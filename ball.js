8cf532e4e502f7844c2b6e2244e42ed6e3e5047f
DS83969 <zhannagolubeva@yahoo.com>
2022-03-24 17:25:46 -0500

    MonaLisa painting

---
+++ ball.js
@@ -0,0 +1,13 @@
+var length = data.length;
+
+// you work goes here
+// -----------------------
+
+
+while (data.i = 0, i < length) {
+    var ball = data[i];
+    create (ball.x, ball.y, ball.color);
+    i ++;
+} 
+
+create();
